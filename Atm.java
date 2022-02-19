d489d205f88f950a8ec4307303d3aaa803e249e2
Aswath RA <93179993+Aswath-RA@users.noreply.github.com>
2022-02-19 10:23:43 -0500

    updated some auth lines

--- Atm.java
+++ Atm.java
@@ -7,74 +7,93 @@ import java.sql.Statement;
 import java.util.Scanner;
 import java.sql.*;
 public class Atm {
+	
 	//String First_Choicer;
+	//Variable for Auth and choice
 	int Accno ;
 	int pin;
 	int option;
 	
-	
 	Scanner string =new Scanner(System.in);
 	Scanner integer = new Scanner(System.in);
-
+	
+	//DB Connection Variables
+	String url = "jdbc:mysql://localhost:3306/Atm?characterEncoding=utf8";
+	String username ="blackfox";
+	String password ="11aa22**";
+	ResultSet rs;
+	PreparedStatement st;
+	String qry ="";
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
 		
 			Atm obj = new Atm();
-			obj.display();
-		
+			obj.display();	
 		
 	}
 	
-		
-		
-	
+			
 	public void display () throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");  
+		Connection con = DriverManager.getConnection(url,username,password);
+		Statement stmt = con.createStatement();
 		
 		System.out.println(" \t\t\t\t\t\t\t\t***************************************");
 		System.out.println(" \t\t\t\t\t\t\t\t********     Welcome      *************");
 		System.out.println(" \t\t\t\t\t\t\t\t********      To          *************");
 		System.out.println(" \t\t\t\t\t\t\t\t******** RAVIJI_ASWA ATM  *************");
 		System.out.println(" \t\t\t\t\t\t\t\t***************************************");
-		System.out.println("\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t Press ENTER to Continue");
+		System.out.println("\n\n\t\t\t\t\t\t\t Press ENTER to Continue");
 		string.nextLine();
 		System.out.println("*****Enter Your Credintals******\n\n");
-		System.out.print("ACCOUNT NO: ");
-		Accno=integer.nextInt();
+		int flag = 0;
+		while (flag < 3) {
 		
-		System.out.print("PIN NO:     ");
+		System.out.println("Enter your Account Number: ");
+		Accno=integer.nextInt();
+		System.out.println("Enter your Pin: ");
 		pin=integer.nextInt();
 		
-		if (Accno ==9629638 && pin ==9796) {
+		qry = "SELECT * from Atmusers where Accno = "+Accno+" AND Pin = "+pin+" ";
+		rs=stmt.executeQuery(qry);
+		
+		if (rs.next()) {
 			System.out.println("Your Account is Valid :-)\n\n");
 			options();
+			break;	
 		}
 		else {
 			System.out.println(" Sorry ,your Account is IN-Valid :-(\n");
-
+			flag = flag + 1;
+		}
+		
+		if (flag == 3){
+			System.out.println(" \n..............................");
+			System.out.println(" Please Try after some time ...");
+			System.out.println(" ..............................\n");	
+			thankyou();
+		}
 		}
 	}
 	
 	public void options() throws Exception{
 		
 		Class.forName("com.mysql.jdbc.Driver");  
-		String url = "jdbc:mysql://localhost:3306/Atm?characterEncoding=utf8";
-		String username ="blackfox";
-		String password ="11aa22**";
 		Connection con = DriverManager.getConnection(url,username,password);
 		Statement stmt = con.createStatement();
 		ResultSet rs;
 		PreparedStatement st;
 		String qry ="";
+
 		System.out.println("--------------");
 		System.out.println("ATM Working");
-		System.out.println("--------------");
-		
+		System.out.println("--------------\n");
 		
 		System.out.println("-----------------------");
 		System.out.println("Welcome to Atm Options");
-		System.out.println("------------------------");
+		System.out.println("------------------------\n\n");
 
+		System.out.println("----------------------------");
 		System.out.println("Type 1 : Deposit");
 		System.out.println("Type 2 : Withdraw");
 		System.out.println("Type 3 : Balance check");
@@ -83,14 +102,15 @@ public class Atm {
 		System.out.println("Type 6 : Contact-US");
 		System.out.println("Type 7 : Exit Out ATM");
 		System.out.print("\nEnter your option 1 to 7 : ");
+		System.out.println("------------------------------");
 
 
 		option=integer.nextInt();
 		switch(option){
 			case 1:
-				System.out.println("-----------");
-				System.out.println("\n Deposit");
-				System.out.println("-----------");
+				System.out.println("\n-----------");
+				System.out.println(" Deposit");
+				System.out.println("-----------\n");
 				int deposit;
 				while (true) {
 				System.out.println("Enter Amount to Deposit: ");
@@ -105,16 +125,18 @@ public class Atm {
 				qry = "UPDATE Atmusers set Balance = Balance + "+deposit+" where Accno = "+Accno+" AND Pin = "+pin+" ";
 				st= con.prepareStatement(qry);
 				st.executeUpdate();
+				System.out.println("\n---------------");
 				System.out.println("Amout Deposited");
-			
-				System.out.println("-----------");
-				
+				System.out.println("---------------\n");
+				thankyou();
+
 				break;
+
 			case 2:
 				int withdraw;
-				System.out.println("-----------");
+				System.out.println("\n-----------");
 				System.out.println("Withdraw");
-				System.out.println("-----------");
+				System.out.println("-----------\n");
 				while (true) {
 					System.out.println("Enter Amount to Withdraw: ");
 					withdraw=integer.nextInt();
@@ -128,39 +150,42 @@ public class Atm {
 					qry = "UPDATE Atmusers set Balance = Balance - "+withdraw+" where Accno = "+Accno+" AND Pin = "+pin+" ";
 					st= con.prepareStatement(qry);
 					st.executeUpdate();
-					System.out.println("---Please Collect Your Money From The Machine ---");
-					System.out.println("Thank you for Banking ");
-					System.out.println("-----------");
+					System.out.println("\n------------------------------------------");
+					System.out.println("---Please Collect Your Money From Atm ------");
+					System.out.println("-------------------------------------------\n");
+					thankyou();
 				
 				break;
 			case 3:
-			    System.out.println("-------------");
+			    System.out.println("\n-------------");
 				System.out.println("Balance Check");
-				System.out.println("--------------");
+				System.out.println("--------------\n");
 				int balance;
 				String name; 
 				qry = "SELECT Name,Balance from Atmusers where Accno = "+Accno+" AND Pin = "+pin+" ";
-				
 				rs=stmt.executeQuery(qry);
-				
 				
 				while (rs.next()) {
 					name=rs.getString("Name");
 					balance = rs.getInt("Balance");
 					System.out.println("Hi "+name+" ");
+					System.out.println("\n-------------------------------------");
 					System.out.println("Your Available Balance is : "+ balance);
-					System.out.println("--------------");
+					System.out.println("-------------------------------------\n");
+					thankyou();
 				}
 				break;
+
 			case 4:
-			    System.out.println("--------------");
+			    System.out.println("\n--------------");
 				System.out.println("Mini Statement");
-				System.out.println("--------------");
+				System.out.println("--------------\n");
+				//Enter mini statement code
 				break;
 			case 5:
-				System.out.println("------------");
+				System.out.println("\n------------");
 				System.out.println("Pin Change");
-				System.out.println("------------");
+				System.out.println("------------\n");
 				
 				String checkdob = null;
 				int checkpin = 0;
@@ -170,15 +195,14 @@ public class Atm {
 				while (rs.next()) {
 					checkdob = rs.getString("Dob");
 					checkpin = rs.getInt("Pin");
-					System.out.println(checkdob);
-					System.out.println(checkpin);
+					//System.out.println(checkdob);
+					//System.out.println(checkpin);
 					
 				}
 				int newpin;
 				int oldpin;
 				String dob;
 				Boolean  scheck;
-				
 				
 				System.out.println("Enter Your Old Pin: ");
 				oldpin = integer.nextInt();
@@ -210,32 +234,47 @@ public class Atm {
 					qry = "UPDATE Atmusers set Pin =  "+newpin+" where Accno = "+Accno+" AND Pin = "+pin+" ";
 					st= con.prepareStatement(qry);
 					st.executeUpdate();
+					System.out.println("\n------------------------------");
 					System.out.println("Your New Pin Set Successfully ");
-					System.out.println("------------------------------");
+					System.out.println("------------------------------\n");
+					thankyou();
 					
 				}
 				else {
 					System.out.println(" Your Old pin or Date-of-Birth is Wrong.  ");
-					System.out.println("Try After Some Time \n Thank you ..");
+					System.out.println("Try After Some Time \n ----- Thank you ..");
+					System.out.println("------------------------------------\n");
+					thankyou();
 				}
 				
 			
 				break;
 			case 6:
-				System.out.println("-----------");
+				System.out.println("\n-----------");
 				System.out.println("Contact Us");
-				System.out.println("------------");
+				System.out.println("------------\n");
+				System.out.println("\n-------------------------------");
 				System.out.println("For any banking related Queries");
-				System.out.println("mail to : help@ravijiaswa.com");
+				System.out.println("mail to : help@ravijiaswa.com ");
+				System.out.println("-------------------------------\n");
+				thankyou();
 				break;
 			case 7:
 				//System.out.println("Exit");
 				display();
 				break;
 			default:
-				System.out.println("Enter a number between 1 to 7");
+				System.out.println("\nEnter a number between 1 to 7");
+				System.out.println("-------------------------------\n");
 		}
 
+
+	}
+	public void thankyou(){
+				System.out.println("-----------------------");
+				System.out.println("--Thank You For Using--");
+				System.out.println("-----Black Fox Atm-----");
+				System.out.println("-----------------------");
 
 	}
 }
